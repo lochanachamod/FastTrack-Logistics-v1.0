b98ee1f345ac838c3393c6e29ba8832f35fa9e43
L.B.L.Chamod <lochanachamod7@gmail.com>
2025-06-19 11:31:11 +0530

    Add files via upload

---
+++ ShipMate/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import db.Database;
+import ui.Dashboard;
+
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        Database.initialize();
+        Database.patchShipmentTable();
+        try {
+            Database.connect();
+            System.out.println("✅ SQLite driver loaded and DB connected!");
+        } catch (Exception e) {
+            System.out.println("❌ Database connection failed: " + e.getMessage());
+        }
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("FastTrack Logistics System");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(900, 600);
+            frame.setContentPane(new Dashboard());
+            frame.setVisible(true);
+        });
+    }
+}
